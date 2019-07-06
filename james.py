4b99243334af01d0164c9350d906a21b136e7401
Ryan Fleck <ryan.fleck@protonmail.com>
2019-07-06 17:53:27 -0400

    Update function names.

--- james.py
+++ james.py
@@ -36,7 +36,7 @@ def hello(bot, update):
                 update.message.from_user)))
 
 
-def roll(bot, update):
+def action_roll_die(bot, update):
     logging.debug('Got a ROLL command.')
     result = roll(update.message.text)
     if(result[0]):
@@ -49,7 +49,7 @@ def roll(bot, update):
         update.message.reply_text('The die fell off the table.')
 
 
-def set_role(bot, update):
+def action_set_role(bot, update):
     logging.debug('Got a HELLO command.')
     update.message.reply_text(
         'Hello, {}.\n{}'.format(
@@ -61,8 +61,8 @@ def main():
     # Start the bot.
     updater = Updater(os.getenv('JAMES_TELEGRAM'))
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
-    updater.dispatcher.add_handler(CommandHandler('roll', roll))
-    updater.dispatcher.add_handler(CommandHandler('set_role', set_role))
+    updater.dispatcher.add_handler(CommandHandler('roll', action_roll_die))
+    updater.dispatcher.add_handler(CommandHandler('set_role', action_set_role))
 
     logging.info('Starting JAMES bot.')
     if os.getenv('DYNO'):
