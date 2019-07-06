7961948e194063ac026e3bc8a659a28f80a897da
Ryan Fleck <ryan.fleck@protonmail.com>
2019-07-06 14:21:14 -0400

    Create simple bot.

---
+++ james.py
@@ -0,0 +1,45 @@
+from telegram.ext import Updater, CommandHandler
+import random
+import logging
+import os
+
+# Set debugging level based on environment.
+if os.getenv('DYNO'):
+    logging.basicConfig(
+        format='%(levelname)s - %(message)s',
+        level=logging.INFO)
+else:
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s:  %(name)s - %(message)s',
+        level=logging.DEBUG)
+
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+
+def roll(bot, update):
+    name = update.message.from_user.first_name
+    num = random.randint(0, 21)
+    update.message.reply_text('{} rolled a {}'.format(name, num))
+
+
+# Start the bot.
+updater = Updater(os.getenv('JAMES_TELEGRAM'))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('roll', roll))
+
+logging.info('Starting JAMES bot.')
+if os.getenv('DYNO'):
+    logging.info('Heroku container detected, using WEBHOOKS.')
+    PORT = int(os.environ.get("PORT", "8443"))
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=os.getenv('JAMES_TELEGRAM'))
+    updater.bot.set_webhook(
+        "https://telegram-james.herokuapp.com/{}".format(os.getenv('JAMES_TELEGRAM')))
+else:
+    logging.info('Local env detected, using POLLING.')
+    updater.start_polling()
+    updater.idle()
