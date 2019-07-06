afe90f53baec35af1b52f2a6d8510be3ed40f222
Ryan Fleck <ryan.fleck@protonmail.com>
2019-07-06 16:27:03 -0400

    Improve roll function.

--- james.py
+++ james.py
@@ -2,6 +2,17 @@ from telegram.ext import Updater, CommandHandler
 import random
 import logging
 import os
+import re
+
+'''
+COMMANDS:
+roll - Roll a die. Optionally specify a D-Type (D6, d8, d40)
+set_role - Set user role.
+hello - Test the bot.
+'''
+
+# Regex for all functions:
+d6_regex = re.compile(r'((d|D)(\d+))')
 
 # Set debugging level based on environment.
 if os.getenv('DYNO'):
@@ -15,20 +26,46 @@ else:
 
 
 def hello(bot, update):
+    logging.debug('Got a HELLO command.')
     update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        'Hello, {}. I am JAMES, the Dungeon bot.\n\n{}'.format(
+            update.message.from_user.first_name, str(
+                update.message.from_user)))
 
 
 def roll(bot, update):
+    logging.debug('Got a ROLL command.')
     name = update.message.from_user.first_name
-    num = random.randint(0, 21)
-    update.message.reply_text('{} rolled a {}'.format(name, num))
+    dice = d6_regex.findall(update.message.text)
+    if dice:
+        try:
+            dietype = int(dice[0][2])
+            logging.debug('ROLLing a D' + str(dietype))
+            num = random.randint(1, dietype)
+            update.message.reply_text(
+                '(D{}) {} rolled a {}'.format(
+                    dietype, name, num))
+        except ValueError:
+            update.message.reply_text('Um, that isn\'t a die I know about.')
+
+    else:
+        num = random.randint(1, 20)
+        update.message.reply_text('(D20) {} rolled a {}'.format(name, num))
+
+
+def set_role(bot, update):
+    logging.debug('Got a HELLO command.')
+    update.message.reply_text(
+        'Hello, {}.\n{}'.format(
+            update.message.from_user.first_name, str(
+                update.message.from_user)))
 
 
 # Start the bot.
 updater = Updater(os.getenv('JAMES_TELEGRAM'))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('roll', roll))
+updater.dispatcher.add_handler(CommandHandler('set_role', set_role))
 
 logging.info('Starting JAMES bot.')
 if os.getenv('DYNO'):
@@ -39,6 +76,7 @@ if os.getenv('DYNO'):
                           url_path=os.getenv('JAMES_TELEGRAM'))
     updater.bot.set_webhook(
         "https://telegram-james.herokuapp.com/{}".format(os.getenv('JAMES_TELEGRAM')))
+    updater.idle()
 else:
     logging.info('Local env detected, using POLLING.')
     updater.start_polling()
