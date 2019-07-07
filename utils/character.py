33d506fe1eea403b83718e8755c5bf137807b8f5
Ryan Fleck <ryan.fleck@protonmail.com>
2019-07-06 23:29:34 -0400

    add date to character class

--- utils/character.py
+++ utils/character.py
@@ -1,5 +1,6 @@
 import logging
 import unittest
+import datetime
 
 '''
 The Character class stores all information about player and non-player characters.
@@ -14,9 +15,15 @@ class Character:
     def __init__(self, identity, name):
 
         # Instance variables unique to each instance.
-        self.inventory = {}
+
         self.realname = name
         self.charactername = name
+        self.init_date = datetime.datetime.now()
+
+        self.inventory = {}
+        self.health = 100
+        self.stamina = 100
+        self.magicka = 10
 
 
 '''
@@ -28,9 +35,9 @@ class TestCharacter(unittest.TestCase):
 
     def test_init_character(self):
         logging.info("Setting up a character.")
-        max = Character('7850928375', 'Max')
-        self.assertEqual(max.realname, 'Max')
-        self.assertEqual(max.health, 100)
+        maxine = Character('7850928375', 'Max')
+        self.assertEqual(maxine.realname, 'Max')
+        self.assertEqual(maxine.health, 100)
 
 
 if __name__ == '__main__':
